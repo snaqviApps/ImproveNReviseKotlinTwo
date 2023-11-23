41a67d924ed9d9e8588fd4f1ebd0ff2f607576be
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-11-22 20:57:19 -0600

    refactored, still not using value class

--- src/main/kotlin/ghar/learn/pluralsight/puretwo/ValueClasses.kt
+++ src/main/kotlin/ghar/learn/pluralsight/puretwo/ValueClasses.kt
@@ -4,24 +4,24 @@ import java.lang.IllegalArgumentException
 
 fun main(){
     println("Value class demo")
-    val p0 = Person("223-abc", "Hamza")
+    val p0 = Person(Id("223-aba"), GivenName("Hamza"))
 }
 
-class Person(id: String, givenName: String){
+class Person(id: Id, givenName: GivenName){  }
 
+class GivenName(val givenName: String) {
     init {
-        if(id.isEmpty()) throw IllegalArgumentException()
-        // 111-ddd
-        if(!id.matches(Regex("^\\d{3}-[a-zA-Z]{3}\$"))) throw IllegalArgumentException()
         if (givenName.isEmpty()) throw IllegalArgumentException()
         if (givenName.contains(" ")) throw IllegalArgumentException()
     }
 }
 
-class Student(id: String){
+class Student(id: Id){  }
+
+class Id(id: String){
     init {
+        //111-abc
         if(id.isEmpty()) throw IllegalArgumentException()
-        // 111-ddd
         if(!id.matches(Regex("^\\d{3}-[a-zA-Z]{3}\$"))) throw IllegalArgumentException()
     }
 }

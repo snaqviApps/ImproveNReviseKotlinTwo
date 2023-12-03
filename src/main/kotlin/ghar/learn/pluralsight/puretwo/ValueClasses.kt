a38f5ce1cda3ae5ca2753ac149d9ef6952e5ce19
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-12-03 14:07:09 -0600

    value-keyword makes, those classes as primitive-type where possible

--- src/main/kotlin/ghar/learn/pluralsight/puretwo/ValueClasses.kt
+++ src/main/kotlin/ghar/learn/pluralsight/puretwo/ValueClasses.kt
@@ -7,22 +7,34 @@ fun main(){
     val p0 = Person(Id("223-aba"), GivenName("Hamza"))
 }
 
-class Person(val id: Id, givenName: GivenName) {    // still not taking advantage of 'primitive' type, while creating 'Id'-class
+class Person(val id: Id, val givenName: GivenName) {
     fun provideIds(){
-        println("Id: $id")
+        println("Id: ${id.id}")
+    }
+
+    /** accessing properties in the given (value) classes */
+    fun getLength(){
+        givenName.length
     }
 }
 
-class GivenName(val givenName: String) {
+/**
+ * 'value' keyword
+ */
+@JvmInline
+//value class GivenName(var givenName: String, val lastName: String) {   // Limitations of Value-class: no 'var', only One property
+value class GivenName(val givenName: String) {
+    val length : Int
+        get() = givenName.length
     init {
         if (givenName.isEmpty()) throw IllegalArgumentException()
         if (givenName.contains(" ")) throw IllegalArgumentException()
     }
 }
-
 class Student(id: Id){  }
 
-class Id(id: String){
+@JvmInline
+value class Id(val id: String){
     init {
         //111-abc
         if(id.isEmpty()) throw IllegalArgumentException()

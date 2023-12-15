3d35544b866c74d3f87d5cd78d30b68238918845
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-12-14 18:31:31 -0600

    data classes refactored

--- src/main/kotlin/ghar/learn/pluralsight/puretwo/dataclasses/DataClassesTwo.kt
+++ src/main/kotlin/ghar/learn/pluralsight/puretwo/dataclasses/DataClassesTwo.kt
@@ -5,31 +5,46 @@ fun main(){
             "Copy() lets them change properties values (data), that is otherwise not recommended\n" +
             "as 'Data' classes are ideally immutablen\n")
     println("---------------------------------------------\n")
-    val carTwo = CarTwo("Toyota", 2022)
-    val car3 = carTwo.copy(make="Nissan", year = 2020)
 
-    println("Car2: $carTwo")
-    println("Car3 is car2's copy: $car3")
+    val person = Person("Hamza Naqvi", url="abc.com")
+    println("Person info: $person")
 
-    println("\ndestructuring data-object to get original properties back: ")
-    val (makeD, yearD) = car3
-    println("post destructuring: $makeD, $yearD")
-    val (_, yearD2) = car3
-    println("put '_' for unused property in destructuring: , $yearD2")
-    println("using component() call for the destructuring")
-    println("component1: ${car3.component1()}")
+    /**
+     * Object-instantiating using 'copy()-method', that uses properties ONLY from primary constructor
+     * Data can be changed while copying
+     * */
+    val person2 = person.copy(url="msnbc.com")
+    println("person1 copied as person2: $person2")
 
-    // more about destructing
-    println("\n\n------------------------------------")
-    println("more about destructing")
+    /** Destructuring */
+    val (name, urlInfo) = person2
+    println("person2 de-structured, gives info, name: $name, and url: $urlInfo")
+
+
+    /** Destructuring using 'Compunent()' call */
+    println("person1 de-structured, getting properties using component(): ${person2.component1()}, ${person2.component2()}")
+
+    val (_, url) = person2              //de-structuring with skipping a property using '_' instead.
+    println("using '_' to exclude, unused-property, with only getting 'url' that is required: $url")
 }
 
-data class CarTwo (val make: String, val year: Int)
-
-data class Person(var name: String) {
+data class Person(var name: String, val url:String) {
 
     val parts = name.split(" ")
     val namePart1 = parts.component1()
     val namePart2 = parts.component2()
+
+    val uppedNamePart1: String;
+    val uppedNamePart2: String;
+
+    init {
+        uppedNamePart1 = namePart1.uppercase()
+        uppedNamePart2 = namePart2.uppercase()
+    }
+
+    override fun toString(): String {
+        return "Person(uppedNamePart1='$uppedNamePart1', uppedNamePart2='$uppedNamePart2')"
+    }
+
 
 }

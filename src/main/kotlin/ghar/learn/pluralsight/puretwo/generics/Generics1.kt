7ada1239ddad283d0b84513450419df53a01d7db
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-12-17 01:57:03 -0600

    define generic-type class

---
+++ src/main/kotlin/ghar/learn/pluralsight/puretwo/generics/Generics1.kt
@@ -0,0 +1,35 @@
+package ghar.learn.pluralsight.puretwo.generics
+
+import ghar.learn.pluralsight.puretwo.Person
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+fun main(args: Array<String>) {
+    println("about Generics")
+
+    val env: Envelope<GenPerson> = Envelope(GenPerson(), null)
+
+    val names : List<String> = listOf("Zem", "Mashto")
+    val namesEnv: Envelope<List<String>> = Envelope(names, null)    // 'nested' generic-type (List<String>)
+
+    val sqlEnvelope: SqlEnvelope<List<String>> = SqlEnvelope(100, names)
+
+
+}
+
+class Envelope<T> (val result: T?, val message: String?) {
+
+    val timeGenerated: LocalDateTime
+
+    init {
+        timeGenerated = LocalDateTime.now()
+    }
+}
+
+class GenPerson
+
+//class SqlEnvelope<T>(val count: Int, val data: T) where T:List<Any>  // limit the generic-type to 'list'
+class SqlEnvelope<T:List<Any> >(val count: Int, val data: T)    // same as 'above', but now generic-type is limited, unlike 'where' that can put multiple types as generic
+
+
+
